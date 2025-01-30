ada3d00c6d4f86d2cdad06038059c30aafb841dd
nadraharr <nadraharr@gmail.com>
2025-01-30 09:17:58 +0200

    Category, Operation model-validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_many :operations
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,5 @@
 class Operation < ApplicationRecord
   belongs_to :category
+  validates :amount, numericality: { greater_than: 0 }
+  validates :odate, :description, presence: true
 end
