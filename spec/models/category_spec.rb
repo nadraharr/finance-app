2e35e7e91370ec1376e19971f8cdd828f611c363
nadraharr <nadraharr@gmail.com>
2025-02-05 17:57:49 +0200

    Category model spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,10 +1,20 @@
 require "rails_helper"
 
 RSpec.describe Category, type: :model do
+  # describe "validations" do
+  #   category = Category.new(name: "", description: "")
+  #   it "should validate presence of name" do
+  #     expect(category).not_to be_valid
+  #   end
+  # end
+
+  describe "associations" do
+    it { should have_many(:operations) }
+  end
+
   describe "validations" do
-    category = Category.new(name: "", description: "")
-    it "should validate presence of name" do
-      expect(category).not_to be_valid
-    end
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
   end
 end
