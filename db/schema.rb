2cd40e0543e8610349b512ee110cf60a2acf7197
nadraharr <nadraharr@gmail.com>
2025-02-10 04:46:36 +0200

    User model, Users controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_30_070459) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_10_014002) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -26,6 +26,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_30_070459) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_operations_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email", null: false
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "operations", "categories"
