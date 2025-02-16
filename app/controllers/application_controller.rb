d4aae15ad2a86c5d28824b4bfe3bb6c024755d6c
nadraharr <nadraharr@gmail.com>
2025-02-16 05:59:54 +0200

    RecordNotFound handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
 
   private
 
@@ -27,6 +28,10 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized
     redirect_back_or_to root_path, alert: "You are not authorized to perform this action!"
+  end
+
+  def handle_not_found
+    render file: "public/404.html", status: :not_found, layout: false
   end
 
   helper_method :current_user
